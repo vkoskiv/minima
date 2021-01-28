c4f748c77dea02323b21bfb933ff02f9d84b506a
Valtteri Koskivuori <vkoskiv@gmail.com>
2021-01-28 02:15:00 +0200

    Implement a simple kernel panic()
    It prints a message, clears interrupts and halts the CPU forever.

---
+++ kernel/panic.h
@@ -0,0 +1,19 @@
+//
+//  panic.h
+//  minima
+//
+//  Created by Valtteri Koskivuori on 28/01/2021.
+//  Copyright © 2021 Valtteri Koskivuori. All rights reserved.
+//
+
+#pragma once
+
+#include "terminal.h"
+
+static inline void panic(void) {
+	kprint("PANIC - Halt and catch fire.\n");
+	cli();
+	for (;;) {
+		asm("hlt");
+	}
+}
