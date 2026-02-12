feb0f810d8392617aca80c4471f6399efe587cc6
Valtteri Koskivuori <vkoskiv@gmail.com>
2026-02-12 13:32:31 +0200

    kernel: Use freelist for debug page frame allocations
    Just experimenting more with free-lists.

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -34,6 +34,7 @@ void dump_stage0_pd(void) {
 
 extern void pit_initialize(void);
 
+// TODO: pf_alloc() this
 #define ARENASIZE (8*PAGE_SIZE)
 uint8_t stage1_buf[ARENASIZE] = { 0 };
 
@@ -43,6 +44,11 @@ struct pfcontainer {
 	void *page;
 	v_ilist linkage;
 };
+
+static void dump_arena_space_left(v_ma a) {
+	uint32_t bytes_left = a.end - a.beg;
+	kprintf("%iB left\n", bytes_left);
+}
 
 void kernel_main(void) {
 	terminal_init(VGA_WIDTH, VGA_HEIGHT);
@@ -56,8 +62,13 @@ void kernel_main(void) {
 	kprintf("%iK Kernel image at %h-%h\n", kernel_size, kernel_physical_start, kernel_physical_end);
 
 	v_ma arena = v_ma_from_buf(stage1_buf, ARENASIZE);
+	// arena.flags |= V_MA_SOFTFAIL;
+	v_ma_on_oom(arena) {
+		panic("stage1 arena OOM");
+	}
 
 	v_ilist pageframes = V_ILIST_INIT(pageframes);
+	v_ilist freelist = V_ILIST_INIT(freelist);
 
 	dump_phys_mem_stats(arena);
 	kprintf("ESC = dump uptime\n1 = unmap identity\n2 = dump pd\n3 = free pf\n4 = show memory stats\n5 = allocate pf\n");
@@ -88,8 +99,10 @@ void kernel_main(void) {
 			kprintf("Freeing at %h -> ", cont->page);
 			pf_free(cont->page);
 			v_ilist_remove(&cont->linkage);
+			v_ilist_prepend(&cont->linkage, &freelist);
 			size_t allocd = v_ilist_count(&pageframes);
 			kprintf("%i\n", allocd);
+			dump_arena_space_left(arena);
 		}
 			break;
 		case '4': {
@@ -99,12 +112,20 @@ void kernel_main(void) {
 		}
 			break;
 		case '5': {
-			void *page = pf_allocate();
-			struct pfcontainer *cont = v_new(&arena, struct pfcontainer);
-			cont->page = page;
+			struct pfcontainer *cont;
+			if (v_ilist_count(&freelist) && (cont = v_ilist_get_last(&freelist, struct pfcontainer, linkage)))
+				v_ilist_remove(&cont->linkage);
+			else
+				cont = v_new(&arena, struct pfcontainer);
+			if (!cont) {
+				kprintf("arena and freelist full\n");
+				break;
+			}
+			cont->page = pf_allocate();
 			v_ilist_prepend(&cont->linkage, &pageframes);
 			size_t allocd = v_ilist_count(&pageframes);
-			kprintf("Allocated at %h -> %i\n", page, allocd);
+			kprintf("Allocated at %h -> %i\n", cont->page, allocd);
+			dump_arena_space_left(arena);
 		}
 			break;
 		default:
