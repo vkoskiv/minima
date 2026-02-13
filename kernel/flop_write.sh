bf1b2c2047e6beb678c2de7fd38e614f131aea0b
Valtteri Koskivuori <vkoskiv@gmail.com>
2026-02-13 21:27:20 +0200

    kernel: Add ability to generate 720k floppy images
    Pass SECTORS_PER_TRACK to mbr.S to hard-code that value. 9 for 720k, 18
    for 1440k.
    There is apparently also a BIOS interrupt[1] to detect the disk type at
    runtime, but I don't really want to spend my remaining 17 bytes on that.
    
    Bochs doesn't get separate 1440/720 targets, because the only way to
    specify which image to use is to manually poke at .bochsrc, and I'm not
    going to bother with that.
    
    There is also now a new target, `make media`, to generate all possible
    floppy images.
    
    [1]: https://www.ctyme.com/intr/rb-0639.htm

--- kernel/flop_write.sh
+++ kernel/flop_write.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-IP=192.168.1.107
-USER=sysv
-scp minima.img "$USER"@"$IP":
-ssh "$USER"@"$IP" -t dd if=minima.img of=/dev/fd0 bs=512 status=progress
+if [ $# -lt 1 ]; then
+	echo "Usage: $0 <disk_img>"
+	exit 1
+else
+	IP=192.168.1.107
+	USER=sysv
+	scp "$1" "$USER"@"$IP":
+	ssh "$USER"@"$IP" -t dd if="$1" of=/dev/fd0 bs=512 status=progress
+fi
