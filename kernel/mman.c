vkoskiv/minima

commit d4b5f8a0784fb178308643664ed57e7a87a815d1

kernel: Show current->name in panic message

kernel/mman.c
@@ -14,6 +14,7 @@
 #include <vkern.h>
 #include "pfa.h"
 #include "linker.h"
+#include "sched.h"
 
 /* Anatomy of a virtual address
 31                                  0
@@ -273,14 +274,13 @@ void handle_gp_fault(void) {
 	panic("GP FAULT");
 }
 
-// FIXME: Macro
 static void panic_with_regs(const char *type, virt_addr addr, struct pf_regs *r) {
-	panic(
-		"%s, %s %s %s @ %h\n"
+	__panic("","",0,
+		"%s (%s) %s %s %s @ %h\n"
 			"\tedi: %h, esi: %h, ebp: %h, esp: %h,\n"
 			"\tebx: %h, edx: %h, ecx: %h, eax: %h\n"
 			"\terror: %h\n\teip: %h, cs: %h, eflags: %h",
-		type, r->error.user ? "user" : "kernel", r->error.present ? "PV" : "NP", r->error.write ? "write" : "read", addr,
+		type, current->name, r->error.user ? "user" : "kernel", r->error.present ? "PV" : "NP", r->error.write ? "write" : "read", addr,
 		    r->edi, r->esi, r->ebp, r->esp, r->ebx, r->edx, r->ecx, r->eax,
 			r->error.value,
 			r->eip, r->cs, r->eflags);

kernel/panic.c
@@ -10,7 +10,10 @@ static void halt(void) {
 
 void __panic(const char *file, const char *func, uint32_t line, const char *fmt, ...) {
 	if (g_terminal_initialized) {
-		kprintf("PANIC: %s[%s:%i]: ", func, file, line);
+		if (file[0] && func[0])
+			kprintf("PANIC: %s[%s:%i]: ", func, file, line);
+		else
+			kprintf("PANIC: ");
 		va_list args;
 		va_start(args, fmt);
 		kprintf_internal(fmt, args);

commit 686ae15961239f9c43b5236c079d04e2937aedac

kernel: Actually show current name + id in panic()
I added it to the wrong place earlier, now it always shows up.

Fixes: d4b5f8a0784

kernel/mman.c
@@ -14,7 +14,6 @@
 #include <vkern.h>
 #include "pfa.h"
 #include "linker.h"
-#include "sched.h"
 
 /* Anatomy of a virtual address
 31                                  0
@@ -303,11 +302,11 @@ void handle_gp_fault(void) {
 
 static void panic_with_regs(const char *type, virt_addr addr, struct pf_regs *r) {
 	__panic("","",0,
-		"%s (%s) %s %s %s @ %h\n"
+		"%s %s %s %s @ %h\n"
 			"\tedi: %h, esi: %h, ebp: %h, esp: %h,\n"
 			"\tebx: %h, edx: %h, ecx: %h, eax: %h\n"
 			"\terror: %h\n\teip: %h, cs: %h, eflags: %h",
-		type, current->name, r->error.user ? "user" : "kernel", r->error.present ? "PV" : "NP", r->error.write ? "write" : "read", addr,
+		type, r->error.user ? "user" : "kernel", r->error.present ? "PV" : "NP", r->error.write ? "write" : "read", addr,
 		    r->edi, r->esi, r->ebp, r->esp, r->ebx, r->edx, r->ecx, r->eax,
 			r->error.value,
 			r->eip, r->cs, r->eflags);

kernel/panic.c
@@ -1,6 +1,7 @@
 #include "panic.h"
 #include "terminal.h"
 #include "idt.h"
+#include "sched.h"
 
 static void halt(void) {
 	cli();
@@ -11,7 +12,7 @@ static void halt(void) {
 void __panic(const char *file, const char *func, uint32_t line, const char *fmt, ...) {
 	if (g_terminal_initialized) {
 		if (file[0] && func[0])
-			kprintf("PANIC: %s[%s:%i]: ", func, file, line);
+			kprintf("PANIC(%s[%i]): %s[%s:%i]: ", current->name, current->id, func, file, line);
 		else
 			kprintf("PANIC: ");
 		va_list args;