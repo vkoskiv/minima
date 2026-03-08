vkoskiv/minima

commit 57c1674a81799902d30f54d29873d5a8e5a60638

kernel: sched: rip out idle tasks list
Now there's just the runqueue and the stop list. Allocate & free tasks
on-demand instead of preallocating MAX_TASKS.
It's using pf_alloc(), which isn't ideal, but maybe better than having a
fixed maximum # of tasks.

commit @ instanssi 2026

kernel/sched.c
@@ -15,13 +15,12 @@ struct task fake = {
 	.name = "stage0_earlyinit",
 };
 
-struct task *current = &fake;//NULL;
+struct task *current = &fake;
 
 static tid_t last_tid = 0;
 
 V_ILIST(runqueue);
 V_ILIST(stop_queue);
-V_ILIST(tasks);
 
 static void dump_task(struct task *t);
 
@@ -63,7 +62,7 @@ static int reaper(void *ctx) {
 				w->waiting = 0;
 				v_ilist_append(pos, &runqueue);
 			}
-			v_ilist_prepend(&t->linkage, &tasks);
+			kfree(t);
 		}
 		cli_pop();
 	}
@@ -81,11 +80,10 @@ static int do_idle(void *ctx) {
 struct task *idle_task = NULL;
 
 void sched_init(void) {
-	struct task *buf = kmalloc(MAX_TASKS * sizeof(struct task));
-	for (size_t i = 0; i < MAX_TASKS; ++i)
-		v_ilist_prepend(&buf[i].linkage, &tasks);
-	task_create(do_idle, NULL, "kidle", 0);
+	tid_t ret = task_create(do_idle, NULL, "kidle", 0);
+	assert(ret >= 0);
 	idle_task = v_ilist_get_first(&runqueue, struct task, linkage);
+	assert(idle_task);
 	v_ilist_remove(&idle_task->linkage);
 	assert(!current->cli_depth);
 	current = idle_task;
@@ -169,10 +167,9 @@ struct new_task_stack {
 };
 
 tid_t task_create(int (*func)(void *), void *ctx, const char *name, int user_task) {
-	if (!v_ilist_count(&tasks))
+	struct task *new = kmalloc(sizeof(*new));
+	if (!new)
 		return -1;
-	struct task *new = v_ilist_get_first(&tasks, struct task, linkage);
-	v_ilist_remove(&new->linkage);
 	new->name = name;
 	new->waiters = V_ILIST_INIT(new->waiters);
 	new->id = last_tid++;

kernel/include/sched.h
@@ -38,6 +38,5 @@ struct task {
 	int stopping;
 };
 
-#define MAX_TASKS 2000
 #define TASK_STACK_PAGES 1
 extern struct task *current;

commit c4b85a92762cdb0c6a28da3ed9b98419f57307c0

kernel: fix memory leak of task stacks
I removed the reuse of task structures in 57c1674a but forgot to update
the logic for allocating/freeing stack buffers. Free task stack buffers
in kreaper as intended.

Fixes: 57c1674a

kernel/sched.c
@@ -65,6 +65,9 @@ static int reaper(void *ctx) {
 				w->waiting = 0;
 				v_ilist_append(pos, &runqueue);
 			}
+			if (t->stack_user)
+				kfree(t->stack_user);
+			kfree(t->stack_kernel);
 			kfree(t);
 		}
 		cli_pop();
@@ -177,18 +180,14 @@ tid_t task_create(int (*func)(void *), void *ctx, const char *name, int user_tas
 	new->waiters = V_ILIST_INIT(new->waiters);
 	new->id = last_tid++;
 
-	if (!new->stack_kernel) { // Reuse existing stack?
-		new->stack_kernel = kmalloc(2 * TASK_STACK_SIZE);
-		assert(new->stack_kernel);
-		// Can't catch page fault on stack overflow, so work around that by
-		// allocating a redzone that is checked every time the task comes off the
-		// CPU, and kill the task if its stack pointer dips into the redzone.
-		// It's still theoretically possible that a task could overflow its
-		// redzone in a single timeslice. FIXME: Figure out a solution for that maybe.
-		new->redzone_top = (uint8_t *)new->stack_kernel + TASK_STACK_SIZE;
-	} else {
-		kprintf("Reusing stack at %h for task %i\n", new->stack_kernel, new->id);
-	}
+	new->stack_kernel = kmalloc(2 * TASK_STACK_SIZE);
+	assert(new->stack_kernel);
+	// Can't catch page fault on stack overflow, so work around that by
+	// allocating a redzone that is checked every time the task comes off the
+	// CPU, and kill the task if its stack pointer dips into the redzone.
+	// It's still theoretically possible that a task could overflow its
+	// redzone in a single timeslice. FIXME: Figure out a solution for that maybe.
+	new->redzone_top = (uint8_t *)new->stack_kernel + TASK_STACK_SIZE;
 
 	uint32_t cs = user_task ? (GDT_USER_CODE | 3) : GDT_KERNEL_CODE;
 	uint32_t ds = user_task ? (GDT_USER_DATA | 3) : GDT_KERNEL_DATA;