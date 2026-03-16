0e27cb3360139a56ae188a788405aafaaa5482f6
Valtteri Koskivuori <vkoskiv@gmail.com>
2026-03-16 23:08:08 +0200

    kernel: fs: add some locks to block dev API
    Probably not enough, but better than nothing.

--- kernel/fs/dev_block.c
+++ kernel/fs/dev_block.c
@@ -4,14 +4,23 @@
 #include <errno.h>
 
 v_ilist block_devices = V_ILIST_INIT(block_devices);
-//FIXME: lock for device list & individual devices
+static int s_blockdev_initialized = 0;
+struct semaphore s_block_devices_lock;
+
+static void dev_block_init(void) {
+	sem_init(&s_block_devices_lock, 1, "block_devices");
+	s_blockdev_initialized = 1;
+}
 
 int dev_block_get_block_count(struct dev_block *dev) {
 	if (!dev)
 		return -EINVAL;
 	if (!dev->block_count)
 		return -ENOTSUP;
-	return dev->block_count((void *)dev);
+	sem_pend(&dev->blk_sem);
+	int ret = dev->block_count((void *)dev);
+	sem_post(&dev->blk_sem);
+	return ret;
 }
 
 int dev_block_get_block_size(struct dev_block *dev) {
@@ -19,7 +28,10 @@ int dev_block_get_block_size(struct dev_block *dev) {
 		return -EINVAL;
 	if (!dev->block_size)
 		return -ENOTSUP;
-	return dev->block_size((void *)dev);
+	sem_pend(&dev->blk_sem);
+	int ret = dev->block_size((void *)dev);
+	sem_post(&dev->blk_sem);
+	return ret;
 }
 
 int dev_block_read(struct dev_block *dev, unsigned int lba, unsigned char *out) {
@@ -27,7 +39,10 @@ int dev_block_read(struct dev_block *dev, unsigned int lba, unsigned char *out) 
 		return -EINVAL;
 	if (!dev->block_read)
 		return -ENOTSUP;
-	return dev->block_read((void *)dev, lba, out);
+	sem_pend(&dev->blk_sem);
+	int ret = dev->block_read((void *)dev, lba, out);
+	sem_post(&dev->blk_sem);
+	return ret;
 }
 
 int dev_block_write(struct dev_block *dev, unsigned int lba, const unsigned char *in) {
@@ -35,32 +50,58 @@ int dev_block_write(struct dev_block *dev, unsigned int lba, const unsigned char
 		return -EINVAL;
 	if (!dev->block_write)
 		return -ENOTSUP;
-	return dev->block_write((void *)dev, lba, in);
+	sem_pend(&dev->blk_sem);
+	int ret = dev->block_write((void *)dev, lba, in);
+	sem_post(&dev->blk_sem);
+	return ret;
 }
 
 int dev_block_register(struct dev_block *dev) {
+	if (!s_blockdev_initialized)
+		dev_block_init();
+	sem_pend(&s_block_devices_lock);
 	v_ilist_prepend(&dev->base.linkage, &block_devices);
+	sem_post(&s_block_devices_lock);
 	return 0;
 }
 
 int dev_block_unregister(struct dev_block *dev) {
+	if (!s_blockdev_initialized)
+		dev_block_init();
+	sem_pend(&s_block_devices_lock);
 	v_ilist_remove(&dev->base.linkage);
+	sem_post(&s_block_devices_lock);
 	return 0;
 }
 
 struct dev_block *dev_block_open(const char *name) {
-	v_ilist *pos;
-	v_ilist_for_each(pos, &block_devices) {
-		struct device *d = v_ilist_get(pos, struct device, linkage);
-		if (strcmp(d->name, name) == 0) {
-			++d->refs;
-			return (struct dev_block *)d;
+	if (!s_blockdev_initialized)
+		dev_block_init();
+	struct dev_block *device = NULL;
+	sem_pend(&s_block_devices_lock);
+	{
+		v_ilist *pos;
+		v_ilist_for_each(pos, &block_devices) {
+			struct device *d = v_ilist_get(pos, struct device, linkage);
+			if (strcmp(d->name, name) == 0) {
+				if (!d->refs) {
+					struct dev_block *blk = (struct dev_block *)d;
+					sem_init(&blk->blk_sem, 1, d->name);
+				}
+				++d->refs;
+				device = (struct dev_block *)d;
+				break;
+			}
 		}
 	}
-	return NULL;
+	sem_post(&s_block_devices_lock);
+	return device;
 }
 
 void dev_block_close(struct dev_block *dev) {
+	if (!s_blockdev_initialized)
+		dev_block_init();
 	assert(dev);
 	--dev->base.refs;
+	// TODO: sem_pend & teardown
 }
--- kernel/include/fs/dev_block.h
+++ kernel/include/fs/dev_block.h
@@ -3,9 +3,11 @@
 
 #include <vkern.h>
 #include <device.h>
+#include <sync.h>
 
 struct dev_block {
 	struct device base;
+	struct semaphore blk_sem;
 	int (*block_count)(struct device *dev);
 	int (*block_size)(struct device *dev);
 	int (*block_read)(struct device *dev, unsigned int lba, unsigned char *out);
