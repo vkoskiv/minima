86015023276f49cea69ecb215931bbee578be809
Valtteri Koskivuori <vkoskiv@gmail.com>
2026-04-16 16:04:22 +0300

    kernel: add missing types.h

---
+++ kernel/include/types.h
@@ -0,0 +1,23 @@
+#ifndef _MINIMA_TYPES_H_
+#define _MINIMA_TYPES_H_
+
+#include <stdint.h>
+
+typedef uint8_t  u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int8_t  i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+typedef int32_t ssize_t;
+typedef uint32_t time_t;
+typedef uint16_t mode_t;
+typedef uint32_t off_t;
+typedef uint32_t uid_t;
+typedef uint32_t gid_t;
+
+#endif
